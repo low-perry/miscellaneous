ac995662b445292a86ed7b2f1bb31cc21bbfb788
low-perry <dariohaxhiraj@hotmail.com>
2025-08-10 16:10:30 +0200

    Add first solution

---
+++ algos/arrays/conatinsDuplicate.js
@@ -0,0 +1,10 @@
+const containsDuplicate = (nums) => {
+  const numSet = new Set();
+  for (const num of nums) {
+    if (numSet.has(num)) { 
+      return true;
+    }
+    numSet.add(num);
+  }
+  return false;
+}
